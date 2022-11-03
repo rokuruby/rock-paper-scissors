d45c515c27158e1fba8b346ae474dc610d2ae223
tionglee <llltth@gmail.com>
2022-11-03 12:23:59 -0400

    added user prompt function code in JS file

---
+++ game.js
@@ -0,0 +1,34 @@
+//Generates random computer choice and process it as all caps and as a string and stores it in ComChoice
+function getComputerChoice() {
+    const Choices = ["Rock","Paper","Scissors"];
+    const random = Math.floor(Math.random() * Choices.length);
+    let ComChoice = Choices[random];
+    let ComChoiceCaps = ComChoice.toUpperCase();
+    console.log("CPU = " + ComChoiceCaps);
+    
+}
+getComputerChoice();
+
+//prompt() for user to input a variable that is stored as UserCap and process it as all caps as a string
+function userInput() {
+  var userType = prompt("Please type/enter Rock, Paper or Scissors");
+  let userCap = userType.toUpperCase();
+    if (userCap === "ROCK" || userCap === "PAPER" || userCap === "SCISSORS") {
+    alert("Valid input");
+    } else {
+    alert("Invalid input");
+    }
+console.log("User = " + userCap);
+
+if (typeof userCap === 'string') {
+    console.log('userCap is a string');
+  } else {
+    console.log('userCap is not a string');
+  }
+  
+}
+userInput();
+
+//write a playRound() function that uses to compare UserChoice against ComChoice. one by one and store the result and CPU & player score into variables
+//make a new game() function that calls playRound() function and loops it for 5 times each time each time reporting and updating the CPU and player score
+//the game() function ,at the end of the loop report who is the overall winner by comparing the scores..
